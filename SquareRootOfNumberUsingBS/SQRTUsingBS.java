f12632e69deec559636a62e45d9144e27a10114f
SHIVOM <shivompandey636@gmail.com>
2022-04-04 12:14:02 +0530

    SQRT of num using binary search

---
+++ SquareRootOfNumberUsingBS/SQRTUsingBS.java
@@ -0,0 +1,21 @@
+package SquareRootOfNumberUsingBS;
+
+public class SQRTUsingBS {
+
+	public boolean sqrtOfNum(int n) {
+		int l=0;
+		int r=n;
+		while(l<=r) {
+			int mid=l+((r-l)/2);
+			
+			if(mid*mid==n) {
+				return true;
+			}else if(mid*mid<n) {
+				l=mid+1;
+			}else {
+				r=mid-1;
+			}
+		}
+		return false;
+	}
+}
