74869f3b745b666c1fbb1252584610a324f7f9e6
SHIVOM <shivompandey636@gmail.com>
2022-06-20 15:42:44 +0530

    pivotindex

---
+++ array/pivotIndexwhereLeftSumRIghtsum/solution.java
@@ -0,0 +1,18 @@
+package array.pivotIndexwhereLeftSumRIghtsum;
+
+public class solution {
+    
+        public int findMiddleIndex(int[] nums) {
+            final int sum = Arrays.stream(nums).sum();
+    int prefix = 0;
+    
+    for (int i = 0; i < nums.length; ++i) {
+      if (prefix == sum - prefix - nums[i])
+        return i;
+      prefix += nums[i];
+    }
+    
+    return -1;
+        }
+    }
+
