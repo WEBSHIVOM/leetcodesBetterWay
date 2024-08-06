a8a3bf4c8c700cad34407a8a251adb92d0b07654
WEBSHIVOM <shivompandey636@gmail.com>
2024-08-06 20:03:36 +0530

    changes

---
+++ src/main/java/array/medianOfTwoSortedArray/MedianOfTwoSortedArray.java
@@ -0,0 +1,61 @@
+package main.java.array.medianOfTwoSortedArray;
+
+import java.util.Arrays;
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively,
+ return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ */
+public class MedianOfTwoSortedArray {
+
+    public double findMedianSortedArraysBruteForceWay(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[] merged = new int[m + n];
+        int k = 0;
+        for (int i = 0; i < m ; i++) {
+            merged[k++] = nums1[i];
+        }
+        for (int i = 0; i < n ; i++) {
+            merged[k++] = nums2[i];
+        }
+        Arrays.sort(merged);
+
+        int totalLength = merged.length;
+
+        if (totalLength % 2 == 1) {
+            return (double) merged[totalLength / 2];
+        } else {
+            int middle1 = merged[(totalLength-1)/2];
+            int middle2 = merged[totalLength / 2];
+            return (double)(middle2+middle1)/ 2.0;
+        }
+
+    }
+
+//    public double findMedianSortedArraysOptimizedWay(int[] nums1, int[] nums2) {
+//
+//    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2}, nums2 = {3, 4};
+        MedianOfTwoSortedArray medianOfTwoSortedArray = new MedianOfTwoSortedArray();
+        double result = medianOfTwoSortedArray.findMedianSortedArraysBruteForceWay(nums1, nums2);
+
+        System.out.print("result>>>>>>>>>>>>>");
+        System.out.println(result);
+    }
+
+}
